e4acd28b14a99b570382b99fe4f5f3e7d51f3d4d
ivan <s1103326@studenti.univpm.it>
2026-01-07 19:25:06 +0100

    inizio

---
+++ tests/test_data.py
@@ -0,0 +1,9 @@
+from test.data import MyDataset
+
+from torch.utils.data import Dataset
+
+
+def test_my_dataset():
+    """Test the MyDataset class."""
+    dataset = MyDataset("data/raw")
+    assert isinstance(dataset, Dataset)
